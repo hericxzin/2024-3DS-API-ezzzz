e63f7bea6137d6539a7b04d9ebeeb339109de426
hericlys <140723994+hericxzin@users.noreply.github.com>
2024-09-25 12:07:00 -0300

    Add files via upload

---
+++ grafico/dadosGerais.js
@@ -0,0 +1,18 @@
+const url = "https://github.com/hericxzin/github-json.git"
+
+async function visualizarInformacoes() {
+    const res = await fetch(url);
+    const dados = await res.json();
+
+    const notebooksMaisVendidos = dados[0].titulo;
+    const numeroDeVendas = dados[0].vendas;
+
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('caixa-grafico__class');
+    paragrafo.innerText = `Em busca de descobrir quais foram os notebooks mais vendidos: ${notebooksMaisVendidos} com total de vendas de ${numeroDeVendas} nas plataformas: ${plataformas}.`;
+
+    const caixa = document.getElementById('caixa-grafico');
+    caixa.appendChild(paragrafo);
+}
+
+visualizarInformacoes();
